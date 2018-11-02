c0372774506a829f5071529321b19190920b03f4
Mitsuhide Matsuda <genbu2011@live.jp>
2018-11-02 22:02:35 +0900

    add: readme

--- index.test.js
+++ index.test.js
@@ -3,13 +3,13 @@ const fn = require('./index.js');
 describe('index.js test', () => {
   test('The values are the same', () => {
     // given
-    const questions = [1, 2, 5, 7];
+    const questions = [1, 1.7, 5, 7, 100.02651];
 
     // when
     const answers = questions.map(fn);
 
     // then
-    const expects = [4, 16, 88, 172];
+    const expects = [4, 16, 88, 172, 31852];
     for (let i = 0; i < expects.length; i += 1) {
       expect(answers[i]).toBe(expects[i]);
     }
